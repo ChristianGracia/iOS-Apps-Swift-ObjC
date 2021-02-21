8ca8ed81332228fbb0a279d6b27d2564e2186000
Christian Gracia <cgracia@magellanjets.com>
2021-02-21 16:59:47 -0500

    added app

---
+++ simple-app-storyboard-interface/simple-app-storyboard-interface/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  simple-app-storyboard-interface
+//
+//  Created by Christian Gracia on 2/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
