60711901b1f6785dcd9bcbc2aa039e1206c2161b
Christian Gracia <cgracia@magellanjets.com>
2021-02-21 21:53:45 -0500

    Added randomized dice images after pressing roll button

--- DiceRoller/DiceRoller/ViewController.swift
+++ DiceRoller/DiceRoller/ViewController.swift
@@ -2,11 +2,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        
+        diceImageViewOne.image = diceArray[Int.random(in: 0...5)]
+        diceImageViewTwo.image = diceArray[Int.random(in: 0...5)]
+        
+        
+        
+        
+   
+    }
+    
 }
 
