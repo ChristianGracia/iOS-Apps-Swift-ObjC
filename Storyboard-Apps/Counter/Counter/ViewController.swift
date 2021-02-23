9c7f3cf48ea0199190517fbbbfddb4a9bfff5932
Christian Gracia <cgracia@magellanjets.com>
2021-02-22 22:17:34 -0500

    added simple counter swift app

---
+++ Storyboard-Apps/Counter/Counter/ViewController.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var centerLabel: UILabel!
+    
+    var count = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        count = count + 1
+        
+        centerLabel.text = String(count)
+        
+        if count >= 10 {
+            view.backgroundColor = .blue
+        }
+    }
+    
+}
+
